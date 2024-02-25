255d29ddbfffbba38cc85b96163385e8d0144f70
Dhineshbabbu <sec20ad017@sairamtap.edu.in>
2024-02-25 09:44:11 +0530

    created a shell script

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 ```
 git init
-'''
+```
 
-'''
+```
 get add . # Add all the files in the folder
 ```
 ```
 get commit -m "This is my first commit"
+```
+```
+git pull
+```
---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "Creating ENV with python 3.11 version"
+
+python3.11 -m venv env
+
+echo [$(date)]: "Activating the Environment"
+
+source env\\Scripts\\activate
+
+echo [$(date)]: "Installing the dev requirement"
+
+pip install -r requiremenrs.txt
+
+echo [$(date)]: "END"
