a7e4d1aab4f831faeb6ae2ca7eda1b6682b19679
Dhineshbabbu <sec20ad017@sairamtap.edu.in>
2024-04-01 21:37:40 +0530

    data_transformation completd

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -83,14 +83,26 @@ class DataTransformation:
 
             input_feature_train_df=train_df.drop(columns=drop_features,axis=1)
             target_feature_train_df=train_df[target_feature]
+            input_feature_test_df=test_df.drop(columns=drop_features,axis=1)
+            target_feature_test_df=test_df[target_feature]
 
             input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessor_obj.transform(target_feature_train_df)
+            input_feature_test_arr=preprocessor_obj.transform(input_feature_test_df)
 
             logging.info('Applying column transformation on training and testing data has been completed')
 
+            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
             save_object(file_path=self.data_transformation_config.perprocessor_obj_file_path,
                         obj=preprocessor_obj)
+            
+            logging.info('Preprocessing pickle file saved')
+
+            return(
+                train_arr,
+                test_arr
+            )
 
 
         except Exception as exp:
