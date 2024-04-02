34ffaf736afed1a08740f8b2cb4de7521f226770
Dhineshbabbu <sec20ad017@sairamtap.edu.in>
2024-04-02 14:37:13 +0530

    added training_pipeline

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,4 +1,6 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_training import ModelTraining
 import pandas as pd
 import numpy as np
 import os
@@ -8,5 +10,10 @@ from src.DimondPricePrediction.exeception import customException
 
 
 obj_ingestion=DataIngestion()
+train_data_path,test_data_path=obj_ingestion.initiate_data_ingection()
 
-obj_ingestion.initiate_data_ingection()
+data_transform=DataTransformation()
+train_arr,test_arr=data_transform.initial_data_transformation(train_data_path,test_data_path)
+
+model_training_obj=ModelTraining()
+model_training_obj.initiat_model_training(train_arr,test_arr)
